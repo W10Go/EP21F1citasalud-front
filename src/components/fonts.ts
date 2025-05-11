a5f3fb9eaf19abb9ef1d4bc01317731fce3e9ca5
W10Go <diegoa.carvajal@udea.edu.co>
2025-05-10 20:03:55 -0500

    Se agrega la pagina de login

---
+++ src/components/fonts.ts
@@ -0,0 +1,10 @@
+import { Roboto, Montserrat } from "next/font/google";
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+});
+export const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-roboto",
+});
